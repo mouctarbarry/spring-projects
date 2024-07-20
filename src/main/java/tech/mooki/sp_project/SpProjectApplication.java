5ee6b0027465435c2197e3904c0bd1402f81c6b9
mouctar <contact@mouctar.tech>
2024-07-20 21:48:29 +0200

    adding spring initializer

---
+++ src/main/java/tech/mooki/sp_project/SpProjectApplication.java
@@ -0,0 +1,13 @@
+package tech.mooki.sp_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/mooki/sp_project/SpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.mooki.sp_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
